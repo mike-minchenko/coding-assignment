d9357de7ee8b14c66ecc12a4c3558a86899753a3
Mike Minchenko <t9052585903@gmail.com>
2025-06-05 11:38:40 +0300

    feat: Starred component review

--- src/components/Starred.jsx
+++ src/components/Starred.jsx
@@ -5,7 +5,8 @@ import Movie from './Movie'
 import '../styles/starred.scss'
 
 const Starred = ({viewTrailer}) => {
-
+    // Selecting entire state causes unnecessary re-renders
+    // This selector will trigger component re-render whenever ANY part of state changes
     const state = useSelector((state) => state)
     const { starred } = state
     const { clearAllStarred } = starredSlice.actions
@@ -14,6 +15,7 @@ const Starred = ({viewTrailer}) => {
   return (
     <div className="starred" data-testid="starred">
       {starred.starredMovies.length > 0 && (<div data-testid="starred-movies" className="starred-movies">
+        {/*<h6> is semantically incorrect*/}
         <h6 className="header">Starred movies</h6>
         <div className="row">
         {starred.starredMovies.map((movie) => (
@@ -30,7 +32,9 @@ const Starred = ({viewTrailer}) => {
         </footer>
       </div>)}
 
+      {/*This block can be taken out before returning, so that the option of no films can be worked out first*/}
       {starred.starredMovies.length === 0 && (<div className="text-center empty-cart">
+        {/*aria-hidden=true*/}
         <i className="bi bi-star" />
         <p>There are no starred movies.</p>
         <p>Go to <Link to='/'>Home</Link></p>
